cd7cf5c1d2914aef2b15125a76df07055f02fd8f
Clutchnp <agarwalarnav2007@gmail.com>
2023-12-16 13:53:22 +0530

    adding main file

---
+++ main.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.image import Image
+
+Window.fullscreen = "auto"
+
+class BoardLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.gl = GameLogic()
+
+        for i in range(1, 43):
+            
+            button = Button(pos_hint={"center_x":1, "center_y":1})
+            
+            button.ids["text"] = f"{i}"
+            button.bind(on_press=self.gl.move)
+            self.add_widget(button)
+
+class GameLogic(Widget):
+    def move(self, button):
+        coords = button.pos
+        print(coords)
+        img = Image(source="D:\Coding Files\Python\Prototype\Kivy\Assets\Troops\Knight.png")
+        img.pos = coords
+
+        self.add_widget(img)
+
+class TroopCapture(App):
+    pass
+
+TroopCapture().run()
