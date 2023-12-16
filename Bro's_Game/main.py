06a9b9c5634b0bfd124921fe243cd3a4566c153c
Clutchnp <agarwalarnav2007@gmail.com>
2023-12-16 15:05:32 +0530

    something

--- Bro's_Game/main.py
+++ Bro's_Game/main.py
@@ -1,39 +1,56 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.uix.image import Image
-
-Window.fullscreen = "auto"
+import pygame as pg
+from pygame.locals import *
 
 
-class BoardLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.gl = GameLogic()
+def main():
+    exit = False
+    while not exit:
+        for ev in pg.event.get():
+            if ev.type == pg.QUIT:
+                exit = True
 
-        for i in range(1, 43):
-            button = Button(pos_hint={"center_x": 1, "center_y": 1})
+            if ev.type == pg.KEYDOWN:
+                if ev.key == pg.K_ESCAPE:
+                    exit = True
 
-            button.ids["text"] = f"{i}"
-            button.bind(on_press=self.gl.move)
-            self.add_widget(button)
+        pg.time.Clock().tick(60)
 
+        draw_grid()
 
-class GameLogic(Widget):
-    def move(self, button):
-        coords = button.pos
-        print(coords)
-        img = Image(source="./Knight.png")
-        img.pos = coords
-
-        self.add_widget(img)
+        pg.display.update()
 
 
-class TroopCapture(App):
-    pass
+def get_grid_info():
+    block_size = h // 10
+
+    offset_x = -block_size
+    offset_y = -3 * block_size
+
+    center = (w // 2 + offset_x, h // 2 + offset_y)
+
+    return block_size, center
 
 
-TroopCapture().run()
+def draw_grid():
+    block_size, center = get_grid_info()
+
+    pg.draw.circle(win, "red", (w // 2, h // 2), 10)
+
+    for x in range(center[0], center[0] + (7 * block_size), block_size):
+        for y in range(center[1], center[1] + (6 * block_size), block_size):
+            rect = pg.Rect(x, y, block_size, block_size)
+            pg.draw.rect(win, "white", rect, 1)
+
+
+if __name__ == "__main__":
+    pg.init()
+
+    h = 1080
+    w = 1980
+
+    win = pg.display.set_mode((w, h), pg.FULLSCREEN)
+    pg.display.set_caption("Pygame Test")
+
+    w, h = pg.display.get_surface().get_width(), pg.display.get_surface().get_height()
+
+    main()
