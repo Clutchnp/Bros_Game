3904bd41c94c4bf997bc1faa25f982239fb648e7
Clutchnp <agarwalarnav2007@gmail.com>
2023-12-16 14:14:51 +0530

    pygame_revamp

--- Bro's_Game/main.py
+++ Bro's_Game/main.py
@@ -8,29 +8,32 @@ from kivy.uix.image import Image
 
 Window.fullscreen = "auto"
 
+
 class BoardLayout(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.gl = GameLogic()
 
         for i in range(1, 43):
-            
-            button = Button(pos_hint={"center_x":1, "center_y":1})
-            
+            button = Button(pos_hint={"center_x": 1, "center_y": 1})
+
             button.ids["text"] = f"{i}"
             button.bind(on_press=self.gl.move)
             self.add_widget(button)
+
 
 class GameLogic(Widget):
     def move(self, button):
         coords = button.pos
         print(coords)
-        img = Image(source="D:\Coding Files\Python\Prototype\Kivy\Assets\Troops\Knight.png")
+        img = Image(source="./Knight.png")
         img.pos = coords
 
         self.add_widget(img)
 
+
 class TroopCapture(App):
     pass
+
 
 TroopCapture().run()
